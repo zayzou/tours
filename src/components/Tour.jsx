a671c08ef0f2cff8807c8a2f73b36db37c214e90
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-22 21:55:23 -0400

    Improve Tour component

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -5,11 +5,11 @@ const Tour = ({ id, name, price, info, image, removeTour }) => {
   return (
     <article  className="single-tour">
       <img src={image} alt={name} className="img" />
-      <span className="tour-price">{price}</span>
+      <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
         <p>
-          {readMore ? info : info.substring(0, 200) + "..."}{" "}
+          {readMore ? info : `${info.substring(0, 200)}  ...`}{" "}
           <button className="info-btn" onClick={() => setReadMore(!readMore)}>
             {" "}
             {readMore ? "show less" : "read more" }
