a8332f750d1edd6e8f9fe14b11d77dcc58a145f9
Soffi Zahir <39215907+Soffi-Zahir@users.noreply.github.com>
2023-10-22 22:21:14 -0400

    Edit name :)

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,46 @@ const App = () => {
     console.log(id);
     setTours(tours.filter((tour) => tour.id != id));
   };
+
+  const fetchTours = () => {
+    setIsLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setTours(data);
+        setIsLoading(false);
+      });
+  };
   useEffect(() => {
     setTimeout(() => {
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          setTours(data);
-          setIsLoading(false);
-        });
-    }, 500);
+      fetchTours();
+    }, 0);
   }, []);
+
+  if (isLoading) {
+    return (
+      <main>
+        {" "}
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main>
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <Tours tours={tours} removeTour={removeTour} />
-      )}
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 };
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -3,7 +3,7 @@ const Tours = ({ tours, removeTour }) => {
   return (
     <section>
       <div className="title">
-        <h2>Our Tours</h2>
+        <h2>Sabrina </h2>
         <div className="title-underline"></div>
       </div>
       <div className="tours">
